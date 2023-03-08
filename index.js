d072bf4495684fdbcbc2281cbcee3326b4324a08
yixinsamgong <sam19931225@gmail.com>
2023-03-08 08:12:26 -0500

    API fetch successful

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+let chatGPT = async (prompt) => {
+  const response = await openai. createChatCompletion ({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: prompt }],
+        })
+    return response;
+};
+  chatGPT ("Hello, how are you?")
+      .then((response) => {
+      console.log(response["data"] ["choices"] [0] ["message"] ["content"]) ;
+      })
+      .catch((err) => {
+        console.error (err);
+})
