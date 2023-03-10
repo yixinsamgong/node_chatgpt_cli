cdc364e74bc57a2f0ae3c40cc568907bf15a56fb
yixinsamgong <sam19931225@gmail.com>
2023-03-10 10:18:19 -0500

    yay it works

--- index.js
+++ index.js
@@ -7,17 +7,28 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-let chatGPT = async (prompt) => {
-  const response = await openai. createChatCompletion ({
+
+const chatGPT = async (prompt) => {
+  const response = await openai.createChatCompletion ({
           model: "gpt-3.5-turbo",
           messages: [{ role: "user", content: prompt }],
         })
-    return response;
+    return response
+}
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const askQuestion = () => {
+  readline.question('> ', async (input) => {
+    const response = await chatGPT(input);
+      console.log(response["data"]["choices"][0]["message"]["content"]) ;
+    askQuestion();
+  });
 };
-  chatGPT ("Hello, how are you?")
-      .then((response) => {
-      console.log(response["data"] ["choices"] [0] ["message"] ["content"]) ;
-      })
-      .catch((err) => {
-        console.error (err);
-})
+
+console.log('Welcome to ChatGPT! Type your message below:');
+askQuestion();
